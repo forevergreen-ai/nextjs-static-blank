45e99f54c64e515b583212ad92cc0b8082f4136f
MattVLC <102999348+MattVLC@users.noreply.github.com>
2025-11-29 23:51:32 -0500

    Some message

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white text-black">
+      <h1 className="text-4xl font-bold">Let's Start Building</h1>
+    </div>
+  );
+}
